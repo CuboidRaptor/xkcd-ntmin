ae31b58944208ffc5fccb2d8ca4e1a0b788f282e
Cuboid <fanjas112358@gmail.com>
2025-01-08 21:38:29 -0500

    font, unixtime, localStorage
    funny font
    also API uses unix timestamp now
    weather data is cached in localStorage (I will make it timed later)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ function round(n, d) {
     return Math.round((n + Number.EPSILON) * (10 ** d)) / (10 ** d);
 }
 
+let weatherdata;
+
 function displayweather(wobj) {
     console.log(wobj);
 }
@@ -9,24 +11,29 @@ function displayweather(wobj) {
 function weather(lat, long) {
     fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}\
-&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,weather_code`
+&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,weather_code&timeformat=unixtime`
     ).then(
         (response) => (response.json())
     ).then(
         (data) => {
-            displayweather(data.current);
+            weatherdata = data.current;
+            localStorage.setItem("xkcd-ntmin-wd", JSON.stringify(weatherdata));
+            displayweather(weatherdata);
         }
     )
 }
 
 if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition((position) => {
-        weather(
-            round(position.coords.latitude, 2),
-            round(position.coords.longitude, 2)
-        );
-    });
+    weatherdata = JSON.parse(localStorage.getItem("xkcd-ntmin-wd"));
 
+    if (weatherdata === null) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            weather(
+                round(position.coords.latitude, 2),
+                round(position.coords.longitude, 2)
+            );
+        });
+    }
 }
 else {
     console.log("geolocation not available in navigator");
