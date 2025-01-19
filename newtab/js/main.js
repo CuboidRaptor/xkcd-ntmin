556f83f5156097d50f98c64fe65f4649447b8077
Cuboid <fanjas112358@gmail.com>
2025-01-19 17:11:18 -0500

    nuking weather
    average minimalist
    in reality chrome's location API is just so utterly dogshit that I cannot be bothered to deal with the thirty different errors it spews out at random times
    also I barely look at the weather anyways

--- newtab/js/main.js
+++ newtab/js/main.js
@@ -1,7 +1,3 @@
-function round(n, d) {
-    return Math.round((n + Number.EPSILON) * (10 ** d)) / (10 ** d);
-}
-
 function displayxkcd(data) {
     let title = document.getElementById("xkcdtitle");
     let img = document.getElementById("xkcdimg");
@@ -60,109 +56,11 @@ function xkcdEvent() { // check new xkcd 30 secs after start of every UTC day
     }, (Math.ceil(Date.now() / 86400000) * 86400000 + 30000) - Date.now())
 }
 
-const REFRESHTIME = 60 * 30; // time in seconds of weather refresh, default is half an hour
-let listener = false; // listener that updates weather on focus
-
-function displayWeather(weatherData) {
-    let feelsLike = document.getElementById("wmo"); // display p's based on font size
-    let other = document.getElementById("other");
-    let iconSpan = document.getElementById("wmoiconspan");
-    let icon = document.getElementById("wmoicon");
-    let curWMO = wcjson[weatherData.weather_code][{0: "night", 1: "day"}[weatherData.is_day]]; // wcjson is from wmocodes.js
-
-    feelsLike.innerHTML = `${curWMO.description}`
-    other.innerHTML = `Feels like <b>${weatherData.apparent_temperature}°</b><br>
-Temp: <b>${weatherData.temperature_2m}°</b><br>
-Humidity: <b>${weatherData.relative_humidity_2m}%</b><br>
-Chance of Rain: <b>${weatherData.precipitation_probability}%</b><br>`
-    iconSpan.style.height = "64px"
-    icon.src = "https://openweathermap.org/img/wn/" + curWMO.image;
-    document.getElementById("updating").innerHTML = ""; // remove updating symbol if it exists
-    console.log("DEBUG: Weather display refreshed");
-}
-
-function weather(lat, long) {
-    console.log(`DEBUG: trying to fetch updated weather data at time ${new Date().toISOString()} UTC`);
-    fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}\
-&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,weather_code,is_day\
-&daily=sunrise,sunset&timeformat=unixtime&timezone=auto&forecast_days=1`,
-        {keepalive: true}
-    ).then(
-        (response) => {
-            if (!response.ok) {
-                throw new Error(`Weather API fetch failed with status ${response.status}`);
-            }
-            return response.json();
-        }
-    ).then(
-        (data) => {
-            weatherData = {...data.daily, ...data.current};
-            localStorage.setItem("weatherdata", JSON.stringify(weatherData));
-            displayWeather(weatherData);
-            console.log("DEBUG: Weather data fetched!");
-            console.log(`DEBUG: Weather data last updated at ${weatherData.time}`);
-        }
-    )
-}
-
-function checkWeather() {
-    // check if weather cached, otherwise fetch new
-    if (document.hidden) {
-        if (!listener) {
-            listener = true;
-            document.addEventListener("visibilitychange", visCheck);
-            console.log("DEBUG: visibilitychange event listener set");
-        }
-    }
-    else {
-        if (!("geolocation" in navigator)) {
-            // geolocation unavailable
-            console.log("WARNING: geolocation not available in navigator");
-            return;
-        }
-
-        let weatherData = JSON.parse(localStorage.getItem("weatherdata"));
-
-        if (weatherData !== null) {
-            // data exists
-            displayWeather(weatherData); // display cached data
-            if (weatherData.time > (Date.now() / 1000 - REFRESHTIME)) {
-                // data is recent
-                return;
-            }
-        }
-
-        document.getElementById("updating").innerHTML = "Updating...";
-        navigator.geolocation.getCurrentPosition((position) => {
-            weather(
-                round(position.coords.latitude, 2),
-                round(position.coords.longitude, 2)
-            );
-        }, (error) => {
-            throw new Error(`code ${error.code}, ${error.message}`);
-        }, {timeout: 15000});
-    }
-}
-
-function visCheck() {
-    if (!document.hidden) {
-        checkWeather();
-        document.removeEventListener("visibilitychange", visCheck);
-        listener = false;
-    }
-}
-
 getxkcd();
-checkWeather();
 xkcdEvent();
-
-// refresh every REFRESHTIME seconds and ping the API if necessary
-setInterval(checkWeather, REFRESHTIME * 1000);
 
 // if another tab updates, display the update
 addEventListener("storage", (event) => {
     console.log("DEBUG: localStorage changed detected, syncing data...");
-    displayWeather(JSON.parse(localStorage.getItem("weatherdata")));
     displayxkcd(JSON.parse(localStorage.getItem("xkcddata")).data);
 });
