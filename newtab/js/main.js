3a0b128af381430cad65c01134a8c3f8724adcfd
Cuboid <fanjas112358@gmail.com>
2025-01-20 20:02:57 -0500

    listener fix + placeholder box timing
    whenever storage is changed it queries storage again and refreshes the display (so if it isn't there on page load it'll auto display when it is there)

--- newtab/js/main.js
+++ newtab/js/main.js
@@ -10,31 +10,34 @@ function imgHeightCheck() {
 }
 
 function displayxkcd(data) {
-    let title = document.getElementById("xkcdtitle");
-    let img = document.getElementById("xkcdimg");
-    let imgDiv = document.getElementById("imgdiv");
-    let link = document.getElementById("xkcdlink");
+    chrome.storage.local.get("comicData").then((data) => {
+        if (data.comicData !== undefined) {
+            console.log("DEBUG: XKCD display refreshing...");
+            let title = document.getElementById("xkcdtitle");
+            let img = document.getElementById("xkcdimg");
+            let imgDiv = document.getElementById("imgdiv");
+            let link = document.getElementById("xkcdlink");
+            let cData = data.comicData.data;
 
-    title.innerHTML = data.title;
-    link.href = `https://xkcd.com/${data.num}`;
-    link.innerHTML = "xkcd.com";
-    imgDiv.classList.remove("divinitial");
-    img.src = data.img;
-    img.title = data.alt;
-
-    console.log("DEBUG: XKCD display refreshed");
+            title.innerHTML = cData.title;
+            link.href = `https://xkcd.com/${cData.num}`;
+            link.innerHTML = "xkcd.com";
+            imgDiv.classList.remove("divinitial"); // add placeholder box that will be removed when image loads
+            img.src = cData.img;
+            img.title = cData.alt;
+        }
+        else {
+            console.log("WARNING: Undefined received from chrome.storage.local");
+        }
+    });
 }
 
 // check image height whenever it loads
 document.addEventListener("resize", imgHeightCheck);
 document.getElementById("xkcdimg").addEventListener("load", imgHeightCheck);
 
-chrome.storage.local.get("comicData").then((data) => {
-    if (data.comicData !== undefined) {
-        displayxkcd(data.comicData.data);
-    }
-    else {
-        console.log("WARNING: Undefined received from chrome.storage.local");
-    }
-});
+chrome.storage.local.onChanged.addListener(() => {
+    console.log("DEBUG: chrome.storage.local change detected, refreshing xkcd display...");
+    displayxkcd();
+})
 
